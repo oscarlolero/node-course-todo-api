89328aabcb78d404139ca88acf90f9e2d08311f6
Oscar Montes <o.montescamberos@ugto.mx>
2018-10-28 16:54:41 -0600

    Setup app for Heroku

--- server/db/moongose.js
+++ server/db/moongose.js
@@ -1,9 +1,11 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //para poder manejar promises
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {
+//'mongodb://127.0.0.1:27017/TodoApp'
+mongoose.connect('mongodb://oscarlolero:oscar6932124@ds050077.mlab.com:50077/oscarlolero-todo-app', {
     useNewUrlParser: true
 });
+
 
 module.exports = {
     mongoose
