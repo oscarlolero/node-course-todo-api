a386e6c2273f7c583b019fba8689f3f2e44cafda
Oscar Montes <o.montescamberos@ugto.mx>
2018-10-28 17:30:22 -0600

    Arreglos para local y external host v3

--- server/db/moongose.js
+++ server/db/moongose.js
@@ -3,9 +3,9 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise; //para poder manejar promises
 console.log(process.env.PORT);
 mongoose.connect(!process.env.PORT ? 
-    'mongodb://oscarlolero:oscar6932124@ds050077.mlab.com:50077/oscarlolero-todo-app' 
+    'mongodb://127.0.0.1:27017/TodoApp'
     : 
-    'mongodb://127.0.0.1:27017/TodoApp', {
+    'mongodb://oscarlolero:oscar6932124@ds050077.mlab.com:50077/oscarlolero-todo-app' , {
     useNewUrlParser: true
 });
 
